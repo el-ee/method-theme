4b9440f240ed97701306a5a5c195ce62ad45ffdc
ellie harmon <ellie.harmon@gmail.com>
2014-08-12 08:29:35 -0700

    remove sidebar support

--- functions.php
+++ functions.php
@@ -93,15 +93,18 @@ add_action( 'after_setup_theme', 'method_setup' );
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
 function method_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'method' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
+	// register_sidebar( array(
+//     'name'          => __( 'Sidebar', 'method' ),
+//     'id'            => 'sidebar-1',
+//     'description'   => '',
+//     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+//     'after_widget'  => '</aside>',
+//     'before_title'  => '<h1 class="widget-title">',
+//     'after_title'   => '</h1>',
+//   ) );
+
+// no sidebar. TODO: possibly add make widget area in footer? 
+
 }
 add_action( 'widgets_init', 'method_widgets_init' );
 
